1507f089d4ae930e2d4e37094fddca3630140a89
h5lianfu <h5lianfu@gmail.com>
2024-12-07 17:04:01 -0500

    support https-ing

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
 
 const app = express();
-const PORT = 80;
+const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'public', 'lovekeqi.com.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'public', 'lovekeqi.com_public.crt')),
+};
+
+const server = https.createServer(options, app);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.use(express.static("images"));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Express with TypeScript running: http://localhost:${PORT}`);
+  console.log(`Express with TypeScript server running: http://localhost:${PORT}`);
 });
